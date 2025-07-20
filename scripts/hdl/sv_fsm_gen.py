c4de28bcf8275e1b7ea3b2678f8e75b1fbf9be2f
Jonathan Roa <roajonny@gmail.com>
2025-07-19 20:44:58 -0700

    Updates sv fsm gen script to print states in combinational block up to the parameterized state count

--- scripts/hdl/sv_fsm_gen.py
+++ scripts/hdl/sv_fsm_gen.py
@@ -75,7 +75,7 @@ f.write ("    always_comb begin\n")
 f.write ("        case (r_STATE)\n")
 
 # Print primary states w/ output logic
-for i in range(2**p_STATE_BITS):
+for i in range(p_STATE_COUNT-1):
      f.write ("            s_STATE" + str(i) +": begin\n")
      for i in range(p_OUTPUT_COUNT):
         if i < p_OUTPUT_COUNT-1:
